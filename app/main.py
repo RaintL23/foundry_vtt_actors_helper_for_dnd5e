020cc2c9245d435cd5cffd0b097a5a3bf6a032bd
RaintL23 <miguelzamora.23.97@gmail.com>
2024-06-12 23:27:26 -0300

    Update: miscelanous

--- app/main.py
+++ app/main.py
@@ -1,16 +1,15 @@
 # mi_proyecto/main.py
 from fastapi import FastAPI
-from app.endpoints import external_api_service, items, users
+from app.endpoints import external_api_routes
 from app.actors import npcs
 
 app = FastAPI()
 
 # Incluir los routers de los endpoints
-app.include_router(items.router)
-app.include_router(users.router)
-app.include_router(external_api_service.router)
+app.include_router(external_api_routes.router)
 app.include_router(npcs.router)
 
 @app.get("/")
 def read_root():
+    """."""
     return {"Hello": "World"}
