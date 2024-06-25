104cb81dd814bea68b6dc2c878c9c60f07204cd9
RaintL23 <miguelzamora.23.97@gmail.com>
2024-06-25 16:05:47 -0300

    Update: miscelanoeus

--- app/endpoints/external_api.py
+++ app/endpoints/external_api.py
@@ -1,12 +1,13 @@
+"""."""
 # app/external_api.py
 import httpx
-import app.static.constants as app_constants
 
 
 class ExternalApi:
-
+    """."""
     @staticmethod
     async def get(url: str):
+        """."""
         async with httpx.AsyncClient() as client:
             response = await client.get(url)
             return response.json()
